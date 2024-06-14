7714801e2bc92d9fd138b59ef12939fceeb61f98
doddfatherr <122844613+doddfatherr@users.noreply.github.com>
2024-06-14 10:49:49 -0700

    website v7

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['clear_5fbuffer_0',['clear_buffer',['../main_8c.html#a1fc2847c81c85a46534d696a15bcece7',1,'main.c']]],
-  ['cmsis_1',['CMSIS',['../group___c_m_s_i_s.html',1,'']]],
-  ['command_5fcomplete_2',['command_complete',['../main_8c.html#a9db3f560cf35596b3f6a3af6ab6ce0b2',1,'main.c']]],
-  ['counter_3',['counter',['../main_8c.html#a617a47c70795bcff659815ad0efd2266',1,'main.c']]],
-  ['current_5fx_5floc_4',['current_x_loc',['../main_8c.html#a38b3c6c26751861417802064336c4f3c',1,'main.c']]],
-  ['current_5fy_5floc_5',['current_y_loc',['../main_8c.html#acb9f5c1462795a216d81fc62e95e7551',1,'main.c']]]
+  ['cdc_5ftransmit_5ffs_0',['CDC_Transmit_FS',['../main_8c.html#a5137d6201dbdef2bf351c5b4941c24f4',1,'main.c']]],
+  ['clear_5fbuffer_1',['clear_buffer',['../main_8c.html#a1fc2847c81c85a46534d696a15bcece7',1,'main.c']]],
+  ['cmsis_2',['CMSIS',['../group___c_m_s_i_s.html',1,'']]],
+  ['command_5fcomplete_3',['command_complete',['../main_8c.html#a9db3f560cf35596b3f6a3af6ab6ce0b2',1,'main.c']]],
+  ['counter_4',['counter',['../main_8c.html#a617a47c70795bcff659815ad0efd2266',1,'main.c']]],
+  ['current_5fx_5floc_5',['current_x_loc',['../main_8c.html#a38b3c6c26751861417802064336c4f3c',1,'main.c']]],
+  ['current_5fy_5floc_6',['current_y_loc',['../main_8c.html#acb9f5c1462795a216d81fc62e95e7551',1,'main.c']]]
 ];
--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['clear_5fbuffer_0',['clear_buffer',['../main_8c.html#a1fc2847c81c85a46534d696a15bcece7',1,'main.c']]]
+  ['cdc_5ftransmit_5ffs_0',['CDC_Transmit_FS',['../main_8c.html#a5137d6201dbdef2bf351c5b4941c24f4',1,'main.c']]],
+  ['clear_5fbuffer_1',['clear_buffer',['../main_8c.html#a1fc2847c81c85a46534d696a15bcece7',1,'main.c']]]
 ];
